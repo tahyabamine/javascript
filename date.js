946f6b77dea9ab75f3a3e4554af7473ca3108b45
tahyabamine <95920923+tahyabamine@users.noreply.github.com>
2021-12-16 19:49:17 +0100

    code date

---
+++ date.js
@@ -0,0 +1,5 @@
+const { lstat } = require("fs");
+
+const months =["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+console.log("random month =>", Math.floor(Math. random() * months));lstat
