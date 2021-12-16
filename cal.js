93f17740bcebb7b9fed461050f811869aa8e9b87
tahyabamine <95920923+tahyabamine@users.noreply.github.com>
2021-12-16 15:48:52 +0100

    age comparaotor

---
+++ cal.js
@@ -0,0 +1,17 @@
+
+// let x = 5;
+// let y = 2;
+// let z = x / y;
+// console.log (z);
+// let a = 5;
+// let b = 2;
+// let c = a + b;
+// console.log (c);
+// let d = 5;
+// let e = 2;
+// let f = d * e;
+// console.log (f);
+// let g = 5;
+// let h = 2;
+// let i = g - h;
+// console.log (i);
