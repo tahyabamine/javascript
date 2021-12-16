37004b61bda3d909ebe1d4f4fa68c9de02f50b48
tahyabamine <95920923+tahyabamine@users.noreply.github.com>
2021-12-16 16:25:53 +0100

    age comprator

--- age.js
+++ age.js
@@ -1,1 +1,24 @@
+const d = new Date();
+console.log (d);
+var y= d.getFullYear();
+function age(x) {
+  let z;
+  if (x > y) {
+    z = ':)';
+  } else {
+    z = ':(';
+  }
+  return z;
+}
 
+console.log(age(2000));
+
+
+
+
+
+
+
+
+
+  
